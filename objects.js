741fde5fba1187f51bce91321db8d049ac85d98e
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-07-20 00:28:41 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,8 +1,9 @@
 
-var playlist = {artistName:songTitle}
+var playlist = {artistName:"michael jackson",songTitle:"thriller"}
 
 function updatePlaylist(playlist, artistName, songTitle){
   
-  playlist.assign({},{[name]:title})
+  playlist.assign({},playlist,{[artistName]:songTitle})
+  
   return playlist
 }
